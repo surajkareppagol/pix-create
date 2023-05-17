4f91493704ae8bf5b97cd215bb35180073a2992c
dev-shadoww <dev.shadoww@gmail.com>
2023-05-17 13:22:24 +0530

    commit 11, Refactor code

--- js/index.js
+++ js/index.js
@@ -1,31 +1,17 @@
 "use strict";
 
 /*********************************************
- *  Main
+ *  Navigation
  *********************************************/
 
-const pixels = document.querySelectorAll(".pixel");
-const colors = document.querySelectorAll(".two-color-container__div");
-const tools = document.querySelectorAll(".tool-container__tool");
-
-const selectedTool = document.querySelector(".selected__tool");
-const switchTool = document.querySelector(".selected-color-tool__tool");
-const selectedColorPrimary = document.querySelector(
-  ".selected__color--primary"
-);
-const selectedColorSecondary = document.querySelector(
-  ".selected__color--secondary"
-);
 const detailModalContainer = document.querySelector(".section-detail");
 const detailModalCloseButton = document.querySelector(
   ".detail-container__container--close"
 );
-
 const supportModalContainer = document.querySelector(".section-support");
 const supportModalCloseButton = document.querySelector(
   ".support-container__container--close"
 );
-
 const navTemplate = document.querySelector(".nav-main__li--template");
 const navSupport = document.querySelector(".nav-main__li--support");
 const templateOptionsContainer = document.querySelector(
@@ -34,16 +20,50 @@ const templateOptionsContainer = document.querySelector(
 const templateOptions = document.querySelectorAll(
   ".template-option-container__text"
 );
+
+/*********************************************
+ *  Tool Bar
+ *********************************************/
+
+const tools = document.querySelectorAll(".tool-container__tool");
+
+/*********************************************
+ *  Middle Bar
+ *********************************************/
+
+const pixels = document.querySelectorAll(".pixel");
 const templates = document.querySelectorAll(".template-container");
 const templateSizeEight = document.querySelector(".template-container__8");
 const templateSizeSixteen = document.querySelector(".template-container__16");
+
+/*********************************************
+ *  Side Bar
+ *********************************************/
+
+const colors = document.querySelectorAll(".two-color-container__div");
+const selectedTool = document.querySelector(".selected__tool");
+const switchColorTool = document.querySelector(".selected-color-tool__tool");
+const selectedColorPrimary = document.querySelector(
+  ".selected__color--primary"
+);
+const selectedColorSecondary = document.querySelector(
+  ".selected__color--secondary"
+);
+
+/*********************************************
+ *  Variables
+ *********************************************/
 
 let primaryColor = "#000";
 let secondaryColor = "#fff";
 let currentColor = primaryColor;
 let currentTool = "";
 let currentTemplate = "8";
-let selectedPixels = new Array();
+let selectedPixels = [];
+
+/*********************************************
+ *  Modals
+ *********************************************/
 
 navSupport.addEventListener("click", () => {
   supportModalContainer.classList.remove("hidden");
@@ -53,13 +73,17 @@ detailModalCloseButton.addEventListener("click", () => {
   detailModalContainer.classList.add("hidden");
 });
 
+navTemplate.addEventListener("click", () => {
+  templateOptionsContainer.classList.toggle("hidden");
+});
+
 supportModalCloseButton.addEventListener("click", () => {
   supportModalContainer.classList.add("hidden");
 });
 
-navTemplate.addEventListener("click", () => {
-  templateOptionsContainer.classList.toggle("hidden");
-});
+/*********************************************
+ *  Select Template
+ *********************************************/
 
 templateOptions.forEach((template) => {
   template.addEventListener("click", () => {
@@ -87,7 +111,11 @@ templateOptions.forEach((template) => {
   });
 });
 
-switchTool.addEventListener("click", () => {
+/*********************************************
+ *  Switch Color
+ *********************************************/
+
+switchColorTool.addEventListener("click", () => {
   let tempColor = primaryColor;
   primaryColor = secondaryColor;
   secondaryColor = tempColor;
@@ -95,6 +123,10 @@ switchTool.addEventListener("click", () => {
   selectedColorSecondary.style.backgroundColor = secondaryColor;
   currentColor = primaryColor;
 });
+
+/*********************************************
+ *  Select Tool
+ *********************************************/
 
 tools.forEach((tool) => {
   tool.addEventListener("click", () => {
@@ -105,53 +137,50 @@ tools.forEach((tool) => {
     currentTool = tool.dataset.tool;
     selectedTool.setAttribute("name", tool.getAttribute("name"));
 
-    colors.forEach((color) => {
-      color.addEventListener("click", () => {
-        if (currentColor != color.dataset.color && currentTool === "color") {
-          currentColor = color.dataset.color;
-          secondaryColor = primaryColor;
-          primaryColor = currentColor;
-          selectedColorPrimary.style.backgroundColor = primaryColor;
-          selectedColorSecondary.style.backgroundColor = secondaryColor;
-        }
-
-        if (currentTool === "multi") {
-          selectedPixels.forEach((pixel) => {
-            console.log(color);
-            console.log(pixel);
-            pixel.style.backgroundColor = color.dataset.color;
-            pixel.style.border = "1px solid #000";
-          });
-
-          tool.addEventListener("click", () => {
-            selectedPixels.forEach((pixel) => {
-              pixel.style.border = "1px solid #000";
-            });
-            selectedPixels = new Array();
-          });
-          selectedPixels = new Array();
-        }
-      });
+    selectedPixels.forEach((pixel) => {
+      pixel.style.border = "1px solid #000";
     });
+    selectedPixels = [];
+  });
+});
 
-    pixels.forEach((pixel) => {
-      pixel.addEventListener("click", () => {
-        if (currentTool === "brush") pixel.style.backgroundColor = currentColor;
-        if (currentTool === "erase")
-          pixel.style.backgroundColor = "transparent";
-        if (currentTool === "multi") {
-          selectedPixels.push(pixel);
-          pixel.style.border = "2px solid #0000ff";
+/*********************************************
+ *  Select Color
+ *********************************************/
 
-          tool.addEventListener("click", () => {
-            selectedPixels.forEach((pixel) => {
-              pixel.style.border = "1px solid #000";
-            });
-            selectedPixels = new Array();
-          });
-        }
+colors.forEach((color) => {
+  color.addEventListener("click", () => {
+    if (currentColor != color.dataset.color && currentTool === "color") {
+      currentColor = color.dataset.color;
+      secondaryColor = primaryColor;
+      primaryColor = currentColor;
+      selectedColorPrimary.style.backgroundColor = primaryColor;
+      selectedColorSecondary.style.backgroundColor = secondaryColor;
+    }
+
+    if (currentTool === "multi") {
+      selectedPixels.forEach((pixel) => {
+        pixel.style.backgroundColor = color.dataset.color;
+        pixel.style.border = "1px solid #000";
       });
-    });
+
+      selectedPixels = [];
+    }
+  });
+});
+
+/*********************************************
+ *  Select Pixel
+ *********************************************/
+
+pixels.forEach((pixel) => {
+  pixel.addEventListener("click", () => {
+    if (currentTool === "brush") pixel.style.backgroundColor = currentColor;
+    if (currentTool === "erase") pixel.style.backgroundColor = "transparent";
+    if (currentTool === "multi") {
+      selectedPixels.push(pixel);
+      pixel.style.border = "2px solid #0000ff";
+    }
   });
 });
 
